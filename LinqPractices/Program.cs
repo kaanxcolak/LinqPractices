3f9d5e45960655bea44a7d661a926f2eb828ec59
Kaan Çolak <kaancolak3458@gmail.com>
2023-05-28 01:45:29 +0300

    updated

--- LinqPractices/Program.cs
+++ LinqPractices/Program.cs
@@ -14,6 +14,24 @@ namespace LinqPractices
             //Find()
             Console.WriteLine("***** Find *****");
             var student = _context.Students.Where(student => student.StudentId ==1).FirstOrDefault();  //ilk veriyi getirdi!
+            student = _context.Students.Find(1);
+            Console.WriteLine(student.Name);
+
+            //FirstOrDefault()
+            Console.WriteLine();
+            Console.WriteLine("***** Find *****");
+            student = _context.Students.Where(student => student.Surname == "Arda").FirstOrDefault();
+            Console.WriteLine(student.Name);
+
+            student = _context.Students.FirstOrDefault(x => x.Surname == "Arda"); //First veya Default yaparız çünkü veri yoksa null döndürecek çünkü default da demiş oluyoruz aynı zamanda!
+            Console.WriteLine(student.Name);
+
+            //SingleOrDefault()
+            Console.WriteLine();
+            Console.WriteLine("***** SingleOrDefault *****");
+            student = _context.Students.SingleOrDefault(student => student.Name == "Deniz"); //Tek veri döndüreceğini anlıyoruz!
+
+
 
         }
     }
